aa30d79f9c86781925451bb269851364075d29e4
naoyahirano <hnaoya1120@gmail.com>
2021-09-26 01:11:12 +0900

    Tweet.vueへ遷移するときの"Unexpected token < in JSON at position 1"を解決

--- twitterapp/api/urls.py
+++ twitterapp/api/urls.py
@@ -3,5 +3,5 @@ from twitterapp.api import views
 
 urlpatterns = [
     path('tweets/', views.ListView.as_view(), name="list"),
-    path('tweets/<int:pk>', views.DetailView.as_view(), name="detail"),
+    path('tweets/<int:pk>/', views.DetailView.as_view(), name="detail"),
 ]
